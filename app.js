13f597eac040eaba0fc8f129caa266fcee24b764
yang haotse <ha91252@gmail.com>
2023-04-17 09:22:44 +0800

    feat: add main.hbs/index.hbs/show.hbs render function

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
+const Restaurant = require('./models/restaurant')
+const bodyParser = require("body-parser")
 
 const app = express()
 const port = 3000
 
-app.engine('hbs', exphbs({defaultLayout : 'main'}))
+app.engine('hbs', exphbs({defaultLayout : 'main', extname : '.hbs'}))
 app.set('view engine', 'hbs')
-app.use(express.urlencoded({ extended : true }))
+app.use(express.urlencoded({ extended : true })) //body-parser
+app.use(express.static('public')) //使用靜態檔案
+
 
 if(process.env.NODE_ENV !== 'production'){
   require('dotenv').config()
@@ -24,9 +28,25 @@ db.once('open', () => {
 })
 
 
-app.get('/',(req,res) => {
-  res.send(`happy420`)
+
+
+app.get('/',(req, res) => {
+  // res.render("index")
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch(error => console.log(error))
 })
+app.get('/restaurants/:_id', (req,res) => {
+  const id = req.params._id
+  // console.log(id)
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurants) => res.render("show", { restaurants }))
+    .catch(error => console.log(error))
+  
+})
+
 
 app.listen( port, () => {
   console.log(`Express is running on http://localhost:${port}`)
